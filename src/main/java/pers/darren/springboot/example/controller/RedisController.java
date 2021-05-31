34c0841292c34ab88c0b5b528732d6d8450f0810
Darren Luo <darrenluo1993@163.com>
2021-05-31 16:46:20 +0800

    springboot连接并操作Redis集群

--- src/main/java/pers/darren/springboot/example/controller/RedisController.java
+++ src/main/java/pers/darren/springboot/example/controller/RedisController.java
@@ -2,10 +2,14 @@ package pers.darren.springboot.example.controller;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,5 +27,17 @@ public class RedisController {
     @PostMapping("/getStringKey")
     public String getStringKey(final String key) {
         return this.redisTemplate.opsForValue().get(key);
+    }
+
+    @PostMapping("/rpushListKey")
+    public void rpushListKey(final String key, @RequestParam("list") final List<String> list) {
+        this.redisTemplate.delete(key);
+        this.redisTemplate.opsForList().rightPushAll(key, list);
+    }
+
+    @PostMapping("/rangeListKey")
+    public List<String> rangeListKey(final String key) {
+        final ListOperations<String, String> operations = this.redisTemplate.opsForList();
+        return operations.range(key, 0, operations.size(key));
     }
 }
